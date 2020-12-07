5ddc526409b038af209a713734c3b213f778baaa
Dmitry <xkritikx@gmail.com>
2020-12-07 17:32:25 +0300

    Update app.js
    Using fetch promises chaining and move fetch to valid phone.length block

--- js/app.js
+++ js/app.js
@@ -6,21 +6,29 @@ document.getElementById('submit').onclick = function (event) {
     if (phone.length === 0) {
         document.getElementById('data').innerText = '';
         document.getElementById('errors').innerText = 'Введите не менее 1-го символа';
+    } else if (phone.length > 0) {
         fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
-            body: phone,
             headers: {
                 'Content-Type': 'application/json'
             }
-        })
-    } else if (phone.length > 0) {
-        document.getElementById('data').innerText = `Данные успешно отправлены`;
-        document.getElementById('errors').innerText = '';
-        document.getElementById('phone').value = '';
-        function timeNone() {
-            document.getElementById('data').innerText = '';
-        }
-        setTimeout(timeNone, 3000);
+        }).then(function(response) {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        }).then(function(response) {
+            console.log(response);
+            document.getElementById('data').innerText = `Данные успешно отправлены`;
+            document.getElementById('errors').innerText = '';
+            document.getElementById('phone').value = '';
+            function timeNone() {
+                document.getElementById('data').innerText = '';
+            }
+            setTimeout(timeNone, 3000);
+        }).catch(function(error){
+            console.log(error);
+        });
     } else {
         alert('errors');
     }
